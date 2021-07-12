c22f77cc5442bbaebc1f939ebf2632e8066b2545
ww9322ww <ww932234@163.com>
2021-07-12 08:37:13 +0800

    master

---
+++ myhtml/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="page-box">
+    <img src="http://127.0.0.1:8000/upload/a.jpg" alt="" />
+    <h1>123456456</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default{
+  data(){
+      return{
+        text:''         /* 定义一个函数 text 格式是字符串*/ 
+      }
+  },
+  mounted(){       /*定义一个生命周期*/
+  this.getData()
+},
+  methods:{
+    getData(){
+      axios({
+        method:'post',
+        url: 'http://127.0.0.1:8000/api'
+       
+      }).then((res)=>{
+        console.log(res)
+      }
+      
+      )
+  
+    }
+  },
+}
+</script>
+
+<style>
+#page-box {
+  width: 300px;
+  height: 600px;
+  box-shadow: 0 2px 12px 0 black;
+  margin: 0 auto;
+}
+</style>
